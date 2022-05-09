85993c68908edb2cce799f1398c1b282b0aa6004
Antônio Barboza <antonio.barboza@viacometa.com.br>
2022-05-09 12:11:35 -0700

    refactor: altera icone e cor

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  devServer: {
-      port: 8080,
-      host: '0.0.0.0'
-    }
+	transpileDependencies: ["vuetify"],
+	devServer: {
+		port: 8080,
+		host: "0.0.0.0",
+	},
+	pwa: {
+		themeColor: "#0c0d10",
+		msTileColor: "#000000",
+		iconPaths: {
+			msTileImage: "img/mstile-150x150.png",
+		},
+	},
 };
